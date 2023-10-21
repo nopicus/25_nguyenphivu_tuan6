310a345d2f3a626d56099a3d0783692bee06cc61
nopicus <127013029+nopicus@users.noreply.github.com>
2023-10-21 15:53:12 +0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+import Screen1 from './Pictures/pictures1';
+import Screen2 from './Pictures/pictures2';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    <Screen2/>
+  </>
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
